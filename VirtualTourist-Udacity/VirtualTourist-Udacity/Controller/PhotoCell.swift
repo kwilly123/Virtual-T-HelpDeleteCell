65d94c2f9dfd254aeee3369e74196517e4f8ee51
kwilly123 <61303546+kwilly123@users.noreply.github.com>
2020-03-19 21:56:46 -0400

    Add files via upload

---
+++ VirtualTourist-Udacity/VirtualTourist-Udacity/Controller/PhotoCell.swift
@@ -0,0 +1,40 @@
+//
+//  PhotoCell.swift
+//  VirtualTourist-Udacity
+//
+//  Created by Kyle Wilson on 2020-03-18.
+//  Copyright © 2020 Xcode Tips. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    //Custom xib file to create the cell
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    //Containing the actual image
+    func cellWithImage(imageFetched: UIImage) {
+        loadingIndicator.stopAnimating()
+        loadingIndicator.isHidden = true
+        image.contentMode = .scaleToFill
+        image.image = imageFetched
+    }
+    
+    //Sets cell with placeholder while images load
+    func cellWithPlaceHolder() {
+        loadingIndicator.isHidden = false
+        loadingIndicator.startAnimating()
+        image.contentMode = .scaleToFill
+        image.image = UIImage(named: "placeholder")
+
+    }
+    
+}
